272657b7eea1933f6cb6c1ec5293ece535821942
chrisdvkta <chrisdevkota77@gmail.com>
2025-04-02 12:01:13 +0545

    landing page building

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Open Labs Nepal";
+export const SITE_DESCRIPTION =
+  "Open Labs Nepal is a open source lab work sharing platform built by students for students.";
