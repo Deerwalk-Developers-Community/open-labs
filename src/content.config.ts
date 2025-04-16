d9acdfdc0df7e807d0e619d4c6c654d6207f4c35
Aashutosh Pudasaini <nepalidude3@gmail.com>
2025-04-16 23:21:51 +0545

    infra updates

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,17 +3,34 @@ import { defineCollection, z } from "astro:content";
 
 const universities = defineCollection({
   loader: file("./src/content/universities.yaml"),
+  schema: z.object({
+    name: z.string(),
+  }),
 });
 
 const colleges = defineCollection({
   loader: file("./src/content/colleges.yaml"),
+  schema: z.object({
+    name: z.string(),
+    affiliated_to: z.string(),
+  }),
 });
+
 const courses = defineCollection({
   loader: file("./src/content/courses.yaml"),
+  schema: z.object({
+    name: z.string(),
+    taught_at: z.array(z.string()),
+  }),
 });
 
 const subjects = defineCollection({
   loader: file("./src/content/subjects.yaml"),
+  schema: z.object({
+    name: z.string(),
+    taught_at: z.array(z.string()),
+    courses: z.array(z.string()),
+  }),
 });
 
 export const collections = { universities, colleges, courses, subjects };
