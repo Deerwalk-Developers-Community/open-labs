f370ad349318d5bab02996d63ac8d3f64e89777a
chrisdvkta <chrisdevkota77@gmail.com>
2025-04-20 13:17:27 +0545

    add:college search

---
+++ src/lib/searchCollege.ts
@@ -0,0 +1,33 @@
+export function initCollegeSearch() {
+  document.addEventListener("DOMContentLoaded", () => {
+    const searchInput = document.getElementById("colleges");
+    const searchButton = document.getElementById("search-button");
+
+    const collegeCards = document.querySelectorAll("#college-container");
+
+    function filterColleges() {
+      const searchValue = searchInput?.value.toLowerCase().trim();
+      collegeCards.forEach((card) => {
+        const collegeName = card
+          .querySelector("h3")
+          ?.textContent?.toLowerCase();
+        console.log(collegeName);
+        if (searchValue === "" || collegeName?.includes(searchValue)) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+      });
+    }
+
+    searchButton?.addEventListener("click", filterColleges);
+
+    searchInput!.addEventListener("keyup", (e) => {
+      if (e.key === "Enter") {
+        filterColleges();
+      }
+    });
+
+    // searchInput!.addEventListener("input", filterColleges);
+  });
+}
