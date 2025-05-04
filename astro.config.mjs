5d6f78af43fed15c85e01afad69a87dd3b5a6251
chrisdvkta <chrisdevkota77@gmail.com>
2025-05-04 16:16:19 +0545

    breadcrumbs test

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://deerwalk-developers-community.github.io",
-  base: "open-labs",
+  base: "/open-labs",
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
